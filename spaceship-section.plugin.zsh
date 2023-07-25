9c88c2817fe11757e5104bce3548dd61bb152971
Roy Azran <roy.azran@lemonade.com>
2023-07-25 10:25:02 +0300

    refactor to tower

--- spaceship-section.plugin.zsh
+++ spaceship-section.plugin.zsh
@@ -1,55 +1,51 @@
-#
-# Foobar
-#
-# Foobar is a supa-dupa cool tool for making you development easier.
-# Link: https://www.foobar.xyz
+# Spaceship plugin for Infra Tower
 
 # ------------------------------------------------------------------------------
 # Configuration
 # ------------------------------------------------------------------------------
 
-SPACESHIP_FOOBAR_SHOW="${SPACESHIP_FOOBAR_SHOW=true}"
-SPACESHIP_FOOBAR_ASYNC="${SPACESHIP_FOOBAR_ASYNC=true}"
-SPACESHIP_FOOBAR_PREFIX="${SPACESHIP_FOOBAR_PREFIX="$SPACESHIP_PROMPT_DEFAULT_PREFIX"}"
-SPACESHIP_FOOBAR_SUFFIX="${SPACESHIP_FOOBAR_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"}"
-SPACESHIP_FOOBAR_SYMBOL="${SPACESHIP_FOOBAR_SYMBOL="😍 "}"
-SPACESHIP_FOOBAR_COLOR="${SPACESHIP_FOOBAR_COLOR="yellow"}"
+SPACESHIP_TOWER_SHOW="${SPACESHIP_TOWER_SHOW=true}"
+SPACESHIP_TOWER_ASYNC="${SPACESHIP_TOWER_ASYNC=true}"
+SPACESHIP_TOWER_PREFIX="${SPACESHIP_TOWER_PREFIX="$SPACESHIP_PROMPT_DEFAULT_PREFIX"}"
+SPACESHIP_TOWER_SUFFIX="${SPACESHIP_TOWER_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"}"
+SPACESHIP_TOWER_SYMBOL="${SPACESHIP_TOWER_SYMBOL="😍 "}"
+SPACESHIP_TOWER_COLOR="${SPACESHIP_TOWER_COLOR="yellow"}"
 
 # ------------------------------------------------------------------------------
 # Section
 # ------------------------------------------------------------------------------
 
-# Show foobar status
+# Show tower status
 # spaceship_ prefix before section's name is required!
 # Otherwise this section won't be loaded.
 spaceship_foobar() {
-  # If SPACESHIP_FOOBAR_SHOW is false, don't show foobar section
-  [[ $SPACESHIP_FOOBAR_SHOW == false ]] && return
+  # If SPACESHIP_TOWER_SHOW is false, don't show tower section
+  [[ $SPACESHIP_TOWER_SHOW == false ]] && return
 
-  # Check if foobar command is available for execution
-  spaceship::exists foobar || return
+  # Check if tower command is available for execution
+  spaceship::exists tower || return
 
-  # Show foobar section only when there are foobar-specific files in current
+  # Show tower section only when there are tower-specific files in current
   # working directory.
 
   # spaceship::upsearch utility helps finding files up in the directory tree.
-  local is_foobar_context="$(spaceship::upsearch foobar.conf)"
+  local is_foobar_context="$(spaceship::upsearch tower.conf)"
   # Here glob qualifiers are used to check if files with specific extension are
   # present in directory. Read more about them here:
   # http://zsh.sourceforge.net/Doc/Release/Expansion.html
   [[ -n "$is_foobar_context" || -n *.foo(#qN^/) || -n *.bar(#qN^/) ]] || return
 
-  local foobar_version="$(foobar --version)"
+  local foobar_version="$(tower --version)"
 
   # Check if tool version is correct
   [[ $foobar_version == "system" ]] && return
 
-  # Display foobar section
+  # Display tower section
   # spaceship::section utility composes sections. Flags are optional
   spaceship::section::v4 \
-    --color "$SPACESHIP_FOOBAR_COLOR" \
-    --prefix "$SPACESHIP_FOOBAR_PREFIX" \
-    --suffix "$SPACESHIP_FOOBAR_SUFFIX" \
-    --symbol "$SPACESHIP_FOOBAR_SYMBOL" \
+    --color "$SPACESHIP_TOWER_COLOR" \
+    --prefix "$SPACESHIP_TOWER_PREFIX" \
+    --suffix "$SPACESHIP_TOWER_SUFFIX" \
+    --symbol "$SPACESHIP_TOWER_SYMBOL" \
     "$foobar_version"
 }
